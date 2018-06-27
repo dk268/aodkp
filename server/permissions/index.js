2dead4ae31791bbd59774e5bc003a8039ba7e5be
Danny <Lorddanp@aol.com>
2018-06-26 23:09:46 -0400

    Chore: formatting changed

--- server/permissions/index.js
+++ server/permissions/index.js
@@ -11,27 +11,27 @@
  * To see how these pieces of middleware get used, check out `server/api/users.js`
  */
 const deny = next => {
-  const err = new Error('Not allowed!')
-  err.status = 401
-  next(err)
-}
+  const err = new Error("Not allowed!");
+  err.status = 401;
+  next(err);
+};
 
-const _isLoggedIn = req => !!req.user
+const _isLoggedIn = req => !!req.user;
 
-const _isAdmin = req => req.user.isAdmin
+const _isAdmin = req => req.user.isAdmin;
 
-const _isSelf = req => req.user.id === req.requestedUser.id
+const _isSelf = req => req.user.id === req.requestedUser.id;
 
 module.exports = {
-  isLoggedIn (req, res, next) {
-    _isLoggedIn(req) ? next() : deny(next)
+  isLoggedIn(req, res, next) {
+    _isLoggedIn(req) ? next() : deny(next);
   },
 
-  isAdmin (req, res, next) {
-    _isLoggedIn(req) && _isAdmin(req) ? next() : deny(next)
+  isAdmin(req, res, next) {
+    _isLoggedIn(req) && _isAdmin(req) ? next() : deny(next);
   },
 
-  isSelf (req, res, next) {
-    _isLoggedIn(req) && _isSelf(req) ? next() : deny(next)
-  }
-}
+  isSelf(req, res, next) {
+    _isLoggedIn(req) && _isSelf(req) ? next() : deny(next);
+  },
+};
