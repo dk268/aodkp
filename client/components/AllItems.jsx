b3b183f302bd29e129d435c5ec016d3dcfdbe956
dk268 <lorddanp@aol.com>
2018-07-07 15:09:47 -0400

    Chore(changes): Need to promote Drop to full model. Save point here.

---
+++ client/components/AllItems.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Expander from "./expanders/Expander";
+import { connect } from "react-redux";
+import { getItems } from "../store/allItems";
+import { LOADING, LOADED, ERROR } from "../store";
+import LinearIndeterminate from "./loaders/LinearIndeterminate";
+import { Paper } from "@material-ui/core";
+
+const styles = theme => ({
+  blueBG: {
+    width: "100%",
+    backgroundColor: `lightblue`,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
+
+class AllItems extends Component {
+  componentDidMount = () => {
+    if (this.props.status !== LOADED) this.props.getItems();
+  };
+
+  render = () => {
+    const { classes, allItems } = this.props;
+    switch (this.props.status) {
+      case LOADING:
+        return <LinearIndeterminate />;
+      case ERROR:
+        return <h1> DOOM </h1>;
+      case LOADED:
+        return (
+          <Paper className={classes.blueBG}>
+            <div className="chart">
+              {allItems.map(item => {
+                return <Expander {...this.props} key={item.id} modelName="Item" item={item} />;
+              })}
+            </div>
+          </Paper>
+        );
+      default:
+        return <h1> hit DEFAULT </h1>;
+    }
+  };
+}
+
+Expander.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  status: state.allItems.status,
+  allItems: state.allItems.collection,
+});
+
+const mapDispatchToProps = {
+  getItems,
+};
+
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(AllItems));
---
+++ client/components/expanders/ItemExpander.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const styles = theme => ({
+  grayCard: {
+    display: `flex`,
+    justifyContent: "space-between",
+    backgroundColor: `white`,
+    width: `100%`,
+  },
+  expanderLink: {
+    color: `darkblue`,
+    "&:hover": { color: `green` },
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  typographies: {
+    display: `flex`,
+    justifyContent: `space-around`,
+    maxWidth: `70%`,
+  },
+  bolded: {
+    fontWeight: `bold`,
+  },
+});
+
+const ItemExpander = props => {
+  const { classes, item } = props;
+  return (
+    <ExpansionPanel>
+      <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <div className={classes.grayCard}>
+          <Typography className={classes.heading} component={Link} to={`/items/${item.id}`}>
+            {item.itemName}
+          </Typography>
+          <Typography>{checkpoint.checkpointDKP}</Typography>
+        </div>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails className={classes.typographies}>
+        <Typography>
+          Items dropped:{" "}
+          {checkpoint.items.map(item => (
+            <Typography
+              component={Link}
+              to={`/items/${item.itemId}`}
+              className={classes.expanderLink}
+              key={`item${item.itemId}`}>
+              {item.itemName}
+              {` `}
+            </Typography>
+          ))}
+        </Typography>
+        <Typography>
+          Characters present:{" "}
+          {checkpoint.characters.map(character => (
+            <Typography
+              className={classes.expanderLink}
+              component={Link}
+              to={`/characters/${character.characterId}`}
+              key={`character${character.characterId}`}>
+              {character.characterName}
+              {` `}
+            </Typography>
+          ))}
+        </Typography>
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+};
+
+ItemExpander.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRouter(withStyles(styles)(ItemExpander));
