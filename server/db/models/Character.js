3696019b58bd51a968b6f70c3589d822cb0355e0
dk268 <lorddanp@aol.com>
2018-07-13 13:07:33 -0400

    Feat(models): creates virtual fields for associated models

--- server/db/models/Character.js
+++ server/db/models/Character.js
@@ -1,40 +1,66 @@
 const db = require("../db");
 const Sequelize = require("sequelize");
 
-const Character = db.define("character", {
-  characterName: {
-    type: Sequelize.STRING,
-    allowNull: false,
+const Character = db.define(
+  "character",
+  {
+    characterName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    dkp: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+    isAlt: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
+    altOf: {
+      type: Sequelize.STRING,
+      defaultValue: null,
+    },
+    class: {
+      type: Sequelize.STRING,
+      defaultValue: null,
+    },
+    totalDKPSpent: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+    totalDKPEarned: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+    overflowDKP: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+    associatedCheckpoints: {
+      type: Sequelize.VIRTUAL,
+      defaultValue: [],
+    },
+    associatedDrops: {
+      type: Sequelize.VIRTUAL,
+      defaultValue: [],
+    },
+    associatedItems: {
+      type: Sequelize.VIRTUAL,
+      defaultValue: [],
+    },
   },
-  dkp: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-  },
-  isAlt: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
-  },
-  altOf: {
-    type: Sequelize.STRING,
-    defaultValue: null,
-  },
-  class: {
-    type: Sequelize.STRING,
-    defaultValue: null,
-  },
-  totalDKPSpent: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-  },
-  totalDKPEarned: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-  },
-  overflowDKP: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0,
-  },
-});
+  {
+    hooks: {
+      afterFind: character => {
+        character.associatedDrops = checkpoint.drops ? checkpoint.drops : "not found";
+        character.associatedCheckpoints = checkpoint.checkpoints
+          ? checkpoint.checkpoints
+          : "not found";
+        character.associatedItems = checkpoint.items ? checkpoint.items : "not found";
+      },
+    },
+  }
+);
 
 Character.afterUpdate = function(instance, options) {
   if (instance.dkp > 800) instance.dkp = 800;
