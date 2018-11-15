e8ec1beebded81c1579904b4c9f38ee664665e30
Danny <Lorddanp@aol.com>
2018-11-15 15:48:30 -0500

    Feat(temp): scaffolds date functionality; needs work

--- client/components/expanders/CharacterExpander.jsx
+++ client/components/expanders/CharacterExpander.jsx
@@ -34,7 +34,19 @@ const styles = theme => ({
 });
 
 const CharacterExpander = props => {
-  const { classes, character } = props;
+  const { classes, character, totalCheckpoints } = props;
+  let characterCheckpoints = 0;
+  // console.log("character . checkpoints", character.checkpoints);
+  // console.log(totalCheckpoints);
+  if (character.checkpoints.length) {
+    characterCheckpoints = character.checkpoints.filter(checkpoint => {
+      if (!checkpoint.raid || !checkpoint.raid.raidDate) return false;
+      if (Date.now() - checkpoint.raid.raidDate < 2622000000) return true;
+      return false;
+    }).length;
+  }
+  // console.log(characterCheckpoints, characterCheckpoints / totalCheckpoints);
+
   return (
     <ExpansionPanel>
       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
@@ -49,8 +61,12 @@ const CharacterExpander = props => {
         </div>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails className={classes.typographies}>
-        <Typography>Attendance: 200%</Typography>
-        <Typography>class: {character.class}</Typography>
+        <Typography>
+          {characterCheckpoints && totalCheckpoints
+            ? Math.floor(characterCheckpoints * 10000 / totalCheckpoints) / 100 + `%`
+            : `unavailable`}
+        </Typography>
+        <Typography>class: {character.class ? character.class : `not specified`}</Typography>
         <Typography>DKP spent: {character.totalDKPSpent}</Typography>
         <Typography>DKP earned: {character.totalDKPEarned}</Typography>
         <Typography className={classes.bolded}>{!character.isAlt ? `Main` : `Alt`}</Typography>
