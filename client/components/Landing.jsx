59da6718fa9a800a09e512a3b0b4ce3b44bd481a
Danny <Lorddanp@aol.com>
2018-11-15 10:39:21 -0500

    Feat(landing): adds text to landing page

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Landing = props => {
-  return <h1>Hi!</h1>;
+  return <h1>Welcome! To get started, click a link in the navbar above.</h1>;
 };
 
 export default Landing;
